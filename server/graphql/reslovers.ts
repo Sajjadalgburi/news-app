07e727e436ee1d8ac4525404cbdeac530b991dc0
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-23 01:57:18 -0600

    Add check for empty articles array in displayTrendingArticles resolver

--- server/graphql/reslovers.ts
+++ server/graphql/reslovers.ts
@@ -2,6 +2,12 @@ export const resolvers = {
   Query: {
     displayTrendingArticles: async (_, __, { dataSources }) => {
       const { articles } = await dataSources.articlesAPI.getHomePageArticles();
+
+      // if the articles array is empty, then return empty array
+      if (!articles.length) {
+        return [];
+      }
+
       return articles.map((article) => ({
         ...article,
         image: article.urlToImage,
