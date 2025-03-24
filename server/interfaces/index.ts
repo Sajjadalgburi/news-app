8af168f28f1514d245c3bd203adb8ec77b100278
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-24 01:37:33 -0600

    Remove MyContext interface and add categoryTypes for backend data type annotations

--- server/interfaces/index.ts
+++
@@ -1,5 +0,0 @@
-interface MyContext {
-  token?: string;
-}
-
-export { MyContext };
---
+++ server/types/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * This file will hold all of our backend data types that are needed for type annotations
+ */
+
+export type categoryTypes =
+  | "business"
+  | "entertainment"
+  | "general"
+  | "health"
+  | "science"
+  | "sports"
+  | "technology";
