c960aea90639cc9908dfa3ad8373e32239663625
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-24 01:11:05 -0600

    Refactor ArticlesAPI to update API request URLs for category and search queries

--- server/datasource/index.ts
+++ server/datasource/index.ts
@@ -8,7 +8,9 @@ const apiKey = process.env.NEWS_API_KEY;
  * There are various methods for fetching specific things
  */
 export class ArticlesAPI extends RESTDataSource {
+  // !
   // todo :  make sure to change country back to 'ca'
+  // !
   baseURL = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=us`;
 
   getHomePageArticles() {
@@ -18,23 +20,21 @@ export class ArticlesAPI extends RESTDataSource {
   /**
    * This will essentiall make a call to the news api and make a fetch based on the title of the clicked article
    * @param title
-   * @returns url
    */
   getSingleArticle(title: string) {
-    return this.get(`&q=${title}`);
+    return this.get(`&q=${encodeURIComponent(title)}`);
   }
 
   /**
    * Pass in a specific category and this will return a proper url where we can get the results
    * We can make a method for every category but there is no point. FOLLOW DRY (dont repeat yourself)
    * @param category
-   * @returns
    */
   getCategory(category: categoryTypes) {
-    return this.get(`&category=${category}`);
+    return this.get(`?apiKey=${apiKey}&country=us&category=${category}`);
   }
 
   getArticlesBasedOnSearch(q: string) {
-    return this.get(`&q=${q}`);
+    return this.get(`?apiKey=${apiKey}&country=us&q=${encodeURIComponent(q)}`);
   }
 }
