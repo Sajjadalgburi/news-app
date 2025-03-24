f77126ff403b09081cbbcfbb1c14981dace8f769
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-24 02:32:21 -0600

    Add type definitions for Article, Source, User, and Comment models

---
+++ server/types/models.d.ts
@@ -0,0 +1,33 @@
+export type ArticleModel = {
+  title: string;
+  publishedAt: string;
+  source: string;
+  url: string;
+  urlToImage: string;
+  description?: string;
+  content?: string;
+  image?: string;
+  author?: string;
+  comments?: CommentModel[];
+};
+
+export type SourceModel = {
+  name: string;
+  id?: string;
+};
+
+export type UserModel = {
+  id: string;
+  name: string;
+  email: string;
+  comments?: CommentModel[];
+};
+
+export type CommentModel = {
+  id: string;
+  articleUrl: string;
+  content: string;
+  userId: string;
+  upvote?: number;
+  downvote?: number;
+};
