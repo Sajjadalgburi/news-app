0329a3cea972e9c715fdce5d215bd8ff5c797757
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-24 01:11:18 -0600

    Add category type definition and enhance article fetching logic in resolvers

--- server/graphql/reslovers.ts
+++ server/graphql/reslovers.ts
@@ -1,10 +1,43 @@
+export type categoryTypes =
+  | "business"
+  | "entertainment"
+  | "general"
+  | "health"
+  | "science"
+  | "sports"
+  | "technology";
+
 export const resolvers = {
   Query: {
     displayTrendingArticles: async (_, __, { dataSources }) => {
       const { articles } = await dataSources.articlesAPI.getHomePageArticles();
+      // if the articles array is empty, then return empty array
+      if (!articles.length || articles.length === 0) {
+        return [];
+      }
+
+      console.log(articles[0]);
+
+      return articles.map((article) => ({
+        ...article,
+        image: article.urlToImage,
+      }));
+    },
+
+    getCategory: async (
+      _,
+      {
+        category,
+      }: {
+        category: categoryTypes;
+      },
+      { dataSources },
+    ) => {
+      console.log("API HIT");
+      const { articles } = await dataSources.articlesAPI.getCategory(category);
 
       // if the articles array is empty, then return empty array
-      if (!articles.length) {
+      if (!articles.length || articles.length === 0) {
         return [];
       }
 
