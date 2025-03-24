33d38af8a6ffce06558eab8f0460915f62cd93d0
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-24 02:32:27 -0600

    Add NewsAPIResponse type to enhance type safety for API responses

--- server/types/index.d.ts
+++ server/types/index.d.ts
@@ -2,6 +2,8 @@
  * This file will hold all of our backend data types that are needed for type annotations
  */
 
+import { ArticleModel } from "./models";
+
 export type categoryTypes =
   | "business"
   | "entertainment"
@@ -10,3 +12,12 @@ export type categoryTypes =
   | "science"
   | "sports"
   | "technology";
+
+/**
+ * The type which represents the News API returned object
+ */
+export type NewsAPIResponse = {
+  status: string;
+  totalResults: number;
+  articles: ArticleModel[];
+};
