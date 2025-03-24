6c43175948af7e3af3cdf1b8425cafd960b8a7e2
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-24 02:32:43 -0600

    Update import path for ArticlesAPI in context definition

--- server/context/index.ts
+++ server/context/index.ts
@@ -1,4 +1,4 @@
-import { ArticlesAPI } from "../datasource";
+import { ArticlesAPI } from "../api/datasource";
 
 /**
  * Context type definition that provides type annotations for the resolver context object.
