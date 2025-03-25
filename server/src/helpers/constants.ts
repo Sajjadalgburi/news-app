d309a55fbee2bfa795d0d2997e9ebc87660778aa
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-25 04:29:20 -0600

    Add constants file with fake input for testing ArticleModel

---
+++ server/src/helpers/constants.ts
@@ -0,0 +1,17 @@
+import { ArticleModel } from "../types/models";
+
+/**
+ * Fake input for testing
+ */
+const fakeiInput: ArticleModel = {
+  title: "Test",
+  description: "Test",
+  url: "https://test.com",
+  urlToImage: "https://test.com/image.jpg",
+  publishedAt: new Date().toISOString(), // 2025-03-25T10:23:31.803Z
+  content: "Test",
+  source: {
+    id: "1",
+    name: "Test",
+  },
+};
