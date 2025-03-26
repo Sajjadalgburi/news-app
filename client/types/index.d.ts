bddf93ee1a28ed8433a2664ab4f9fcf7ddc6c05a
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-26 01:07:15 -0600

    Add centralized types for article categories

---
+++ client/types/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Centralized types for the available categories
+ */
+export type categoryTypes =
+  | "business"
+  | "entertainment"
+  | "general"
+  | "health"
+  | "science"
+  | "sports"
+  | "technology";
