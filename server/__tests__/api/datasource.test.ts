1256b4ef04b3712f8329be2e2edf66bcffaecad6
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-25 18:26:59 -0600

    Add initial tests for Article API and database implementations

--- server/__tests__/api/datasource.test.ts
+++ server/__tests__/api/datasource.test.ts
@@ -1,1 +1,6 @@
 // TODO : test the Article API class implementation and its methods so that they all work as expected
+describe.skip("The datasource Article API class testing", () => {
+  it("should do something", () => {
+    expect(true).toBe(true);
+  });
+});
--- server/__tests__/database/db.test.ts
+++ server/__tests__/database/db.test.ts
@@ -1,1 +1,7 @@
 // TODO : test the database implementation so that it works as expected
+
+describe.skip("The database test suit", () => {
+  it("should do something", () => {
+    expect(true).toBe(true);
+  });
+});
