d6b213a8f8193a1f2aa7536fbcce797ed66267d7
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-26 01:07:51 -0600

    Integrate Navbar component into Home page layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/Header/Navbar";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default Home;
