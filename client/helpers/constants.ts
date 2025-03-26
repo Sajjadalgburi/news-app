e4b71cbe55a06165b6e898fbdc321aa74c49274d
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-26 00:26:54 -0600

    Add categories constant for article categorization

---
+++ client/helpers/constants.ts
@@ -0,0 +1,14 @@
+import { categoryTypes } from "@/types";
+
+/**
+ * @type {Array<{ name: categoryTypes }>}
+ */
+export const categories: { name: categoryTypes }[] = [
+  { name: "business" },
+  { name: "technology" },
+  { name: "sports" },
+  { name: "entertainment" },
+  { name: "general" },
+  { name: "health" },
+  { name: "science" },
+];
