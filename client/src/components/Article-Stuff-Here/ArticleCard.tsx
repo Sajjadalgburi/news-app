c8f2ec5711b25b93277d347c7b1fcfa2c41010a4
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-27 00:26:12 -0600

    Refactor ArticleCard component to use generated Article type

--- client/src/components/Article-Stuff-Here/ArticleCard.tsx
+++ client/src/components/Article-Stuff-Here/ArticleCard.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
-interface Article {
-  title: string;
-  image?: string;
-  content: string;
-  author: string;
-  publishedAt: string;
-}
+import { Article } from "@/__generated__/graphql";
 
 interface Props {
   article: Article;
