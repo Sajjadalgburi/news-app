7d9c2767d90e3ac8fcea719ff9c6acbe64722e56
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-27 10:09:27 -0600

    Implement user registration with GraphQL mutation and enhance form validation

--- client/app/(auth-group)/register/page.tsx
+++ client/app/(auth-group)/register/page.tsx
@@ -1,50 +1,64 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import toast from "react-hot-toast";
 import AuthComponent from "@/src/components/AuthComponent";
+import { useMutation } from "@apollo/client";
+import { REGUSTER_NEW_USER } from "@/src/graphql/mutations";
 
 // Define Zod Schema
 export const formSchema = z.object({
-  email: z.string().min(2).max(50),
-  password: z.string().min(8).max(100),
-  username: z.string().min(2).max(50),
+  email: z.string().min(2).max(50).email("Invalid email address").nonempty(),
+  password: z.string().min(6).max(30).nonempty(),
+  username: z.string().min(2).max(50).nonempty(),
+  confirmPassword: z.string().min(6).max(30).nonempty(),
 });
 
 // Infer TypeScript Type from Schema
-export type FormValues = z.infer<typeof formSchema>;
+export type FormValuesRegisterPage = z.infer<typeof formSchema>;
 
 const RegisterPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null | unknown>(null);
-
   // Define form
-  const form = useForm<FormValues>({
+  const form = useForm<FormValuesRegisterPage>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
-      username: "",
     },
   });
 
-  // Handle form submission
-  const onSubmit = async (values: FormValues) => {
+  const [registerMutation, { error, loading }] = useMutation(REGUSTER_NEW_USER);
+
+  const onSubmit = async ({
+    email,
+    password,
+    username,
+  }: FormValuesRegisterPage) => {
     try {
-      setLoading(true);
-      toast.success("You have successfully logged in!");
-      // Handle API call or authentication logic
+      const res = await registerMutation({
+        variables: { email, password, name: username },
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { message, status, token, success } = res.data?.register || {};
+
+      // todo : do something with the token
+      if (status === 400 && !success) {
+        toast.error(message ?? "Registration failed. Please try again.");
+      } else if (status === 200 && success) {
+        toast.success(message ?? "Login successful! Redirecting...");
+      } else {
+        toast.error("Not sure what to do with this yet");
+      }
     } catch (e) {
-      setError(e);
-      toast.error("An error occurred. Please try again.");
+      toast.error(`An error occurred. ${e}`);
     } finally {
-      setLoading(false);
-      setError(null);
-      setLoading(false);
       form.reset();
+      form.setValue("confirmPassword", "");
+      form.setValue("username", "");
     }
   };
 
@@ -53,7 +67,7 @@ const RegisterPage = () => {
       isLoggingIn={false}
       loading={loading}
       error={error}
-      onFormSubmit={onSubmit}
+      onSubmit={onSubmit}
       form={form}
     />
   );
