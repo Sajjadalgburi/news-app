57aa45af63bc23d75c5ea837771bc4c9727233c9
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-27 05:10:14 -0600

    Update ArticlesAPI to fetch articles from a broader range of news outlets and improve date filtering

--- server/src/api/datasource.ts
+++ server/src/api/datasource.ts
@@ -2,35 +2,22 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 import "dotenv/config";
 import { categoryTypes, NewsAPIResponse } from "../types";
 import { ArticleModel } from "../types/models";
+import { getToday, goBackFiveDays } from "../helpers/date-helpers";
 const apiKey = process.env.NEWS_API_KEY;
 
 /**
  * This is the datasource class that will be used and passed as context for graphql.
  * There are various methods for fetching specific things
  */
+
+const newsOutlets =
+  "domains=wsj.com,techcrunch.com,thenextweb.com,bbc.com,cnn.com,nytimes.com,guardian.co.uk,aljazeera.com,reuters.com,forbes.com,bloomberg.com,bbc.co.uk,washingtonpost.com";
 export class ArticlesAPI extends RESTDataSource {
-  baseURL = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=ca`;
+  baseURL = `https://newsapi.org/v2/everything?apiKey=${apiKey}&sortBy=popularity&${newsOutlets}&from=${goBackFiveDays()}&to=${getToday()}`;
 
   async getHomePageArticles(): Promise<ArticleModel[]> {
     const response = await this.get<NewsAPIResponse>("");
     return response.articles;
-  }
-
-  /**
-   * This will essentiall make a call to the news api and make a fetch based on the title of the clicked article
-   * @param title
-   * @returns single array[0]. This is the value closest to the search
-   */
-  async getSingleArticle(title: string): Promise<ArticleModel | null> {
-    const response = await this.get<NewsAPIResponse>(
-      `?apiKey=${apiKey}&country=us&q=${title}`,
-    );
-
-    if (response.status !== "ok" || !response.articles.length) {
-      return null; // Ensures we don't return undefined
-    }
-
-    return response.articles[0]; // Return the first article
   }
 
   /**
@@ -41,7 +28,9 @@ export class ArticlesAPI extends RESTDataSource {
    */
   async getCategory(category: categoryTypes): Promise<ArticleModel[]> {
     const response = await this.get<NewsAPIResponse>(
-      `?apiKey=${apiKey}&country=us&category=${category}`,
+      `?apiKey=${apiKey}&sortBy=popularity&${newsOutlets}&from=${goBackFiveDays()}&to=${getToday()}&q=${encodeURIComponent(
+        category,
+      )}`,
     );
     return response.articles;
   }
@@ -55,7 +44,9 @@ export class ArticlesAPI extends RESTDataSource {
     question: string,
   ): Promise<ArticleModel[] | null> {
     const response = await this.get<NewsAPIResponse>(
-      `?apiKey=${apiKey}&country=us&q=${encodeURIComponent(question)}`,
+      `?apiKey=${apiKey}&sortBy=popularity&from=${goBackFiveDays()}&to=${getToday()}&q=${encodeURIComponent(
+        question,
+      )}`,
     );
 
     if (!response.articles.length || response.status !== "ok") {
