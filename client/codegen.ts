e52fca3817bcbc9ea20d765814f7118a5529ea37
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-26 18:35:27 -0600

    Add GraphQL Codegen configuration for schema and type generation

---
+++ client/codegen.ts
@@ -0,0 +1,21 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  // ! Schema might be pointing to a remote server, so we need to specify the URL
+  schema: "http://localhost:5000/graphql",
+  documents: ["src/**/*.tsx"],
+  generates: {
+    "./__generated__/": {
+      preset: "client",
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+    "./__generated__/types.ts": {
+      plugins: ["typescript", "typescript-operations"],
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
