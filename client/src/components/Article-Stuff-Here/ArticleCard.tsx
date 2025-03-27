4cf30dc1d991b198f49cd6b58d051197db1c1d1e
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-27 02:02:13 -0600

    Refactor ArticleCard to save article before navigation and update link to button

--- client/src/components/Article-Stuff-Here/ArticleCard.tsx
+++ client/src/components/Article-Stuff-Here/ArticleCard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { Article } from "@/__generated__/graphql";
 import { formatDate } from "@/src/helpers";
+import useSaveArticle from "@/src/helpers/saveArticle";
+import { useRouter } from "next/navigation";
 
 interface Props {
   article: Article;
@@ -20,9 +21,30 @@ const ArticleCard: React.FC<Props> = ({
   image = true,
   hasImage,
 }) => {
+  const { loading, saveArticle, error } = useSaveArticle(article);
+  const router = useRouter();
+
+  const handleClick = async (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+
+    try {
+      await saveArticle(); // Ensure article is saved before redirecting
+
+      if (loading && error) {
+        return;
+      } else {
+        router.push(`/article/${encodeURIComponent(article.title)}`);
+      }
+    } catch (error) {
+      console.error("Failed to save article:", error);
+    }
+  };
+
   return (
-    <Link
-      href={`/article/${encodeURIComponent(article.title)}`}
+    <a
+      role="button"
+      aria-disabled={loading}
+      onClick={handleClick}
       className="block overflow-hidden rounded-xl min-h-fit mx-auto self-center shadow-md transition-transform transform hover:scale-102 hover:underline hover:shadow-lg">
       {image && article.image && (
         <div className={`relative w-full ${isFeatured ? "h-96" : "h-48"}`}>
@@ -58,7 +80,7 @@ const ArticleCard: React.FC<Props> = ({
           <span>{formatDate(article.publishedAt)}</span>
         </div>
       </div>
-    </Link>
+    </a>
   );
 };
 
