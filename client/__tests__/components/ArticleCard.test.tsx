4ddf1ccba8cf13041ce1eebc721af2e19a821651
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-28 08:55:45 -0600

    Add initial test setup for ArticleCard component

---
+++ client/__tests__/components/ArticleCard.test.tsx
@@ -0,0 +1,6 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { describe } from "node:test";
+import ArticleCard from "@/components/Article-Stuff-Here/ArticleCard";
+
+describe.todo("ArticleCard Test Case", () => {});
