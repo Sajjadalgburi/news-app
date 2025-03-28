b6a98fa32f270fd3dcd8719418c7b4df86b7d632
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-28 09:40:03 -0600

    Add user details to registration mutation and implement logout mutation

--- client/graphql/mutations.ts
+++ client/graphql/mutations.ts
@@ -15,15 +15,39 @@ export const LOG_USER_IN = gql(`
     message
     token
     status
+    user {
+      id
+      name
+      email
+      profilePicture
+    }
   }
 }`);
 
-export const REGUSTER_NEW_USER =
+export const REGISTER_NEW_USER =
   gql(`mutation RegisterMutation($name: String!, $email: String!, $password: String!) {
   register(name: $name, email: $email, password: $password) {
     success
     message
     status
     token
+    user {
+      id
+      name
+      email
+      profilePicture
+    }
+  }
+}`);
+
+/**
+ * Mutation to logout the user from the application.
+ * This will remove the JWT token from the local storage and set the user to null in the state.
+ */
+export const LOGOUT_USER = gql(`
+  mutation logout {
+  logout {
+    success
+    status
   }
 }`);
