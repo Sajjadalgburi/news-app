011308f75937aeae5424f500695418dd35f6aeb1
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-30 02:04:46 -0600

    Add SingleArticleError component for error handling in article display

---
+++ client/components/Article-Stuff-Here/Single-Article/SingleArticleError.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import { useRouter } from 'next/navigation'
+
+const SingleArticleError = ({ error }: { error: Error | string }) => {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col gap-4 py-5 text-center max-w-screen-lg mx-auto">
+      <p className="text-red-500 text-2xl font-bold">
+        Error: {error instanceof Error ? error.message : error}
+      </p>
+      <Button className='w-fit mx-auto cursor-pointer' variant="outline" onClick={() => router.push("/")}>
+        Go to Home
+      </Button>
+    </div>
+  )
+}
+
+export default SingleArticleError
