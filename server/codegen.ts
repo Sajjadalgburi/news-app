a13269f1d36a48b52e4aafa47fd2c738ee25156e
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-01 07:29:50 -0600

    i hope the app deploys now!

--- server/codegen.ts
+++ server/codegen.ts
@@ -10,7 +10,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
  *
  */
 const config: CodegenConfig = {
-  schema: "./src/graphql/schema.graphql",
+  schema: "./src/graphql/schema.ts",
   generates: {
     "./src/types/types.ts": {
       plugins: ["typescript", "typescript-resolvers"],
