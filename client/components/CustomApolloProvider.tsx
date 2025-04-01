1a364b2d1aac95f05398f91cbda482af21639133
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-31 18:32:17 -0600

    Update Apollo Client URI for environment-specific configuration

--- client/components/CustomApolloProvider.tsx
+++ client/components/CustomApolloProvider.tsx
@@ -11,7 +11,10 @@ import {
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:5000/graphql",
+    uri:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5000/graphql"
+        : `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
     credentials: "include",
   }),
   cache,
