c2f98f26f754a7badd71fcb673f52a4e453d3e31
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-31 19:51:59 -0600

    Configure dotenv for local environment and update GraphQL schema URL logic

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env.local",
+});
+
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   // ! Schema might be pointing to a remote server, so we need to specify the URL
   schema:
-    process.env.NODE_ENV === "development"
+    process.env.NODE_ENV !== "production"
       ? "http://localhost:5000/graphql"
       : `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
   documents: ["./graphql/**/*.ts"],
