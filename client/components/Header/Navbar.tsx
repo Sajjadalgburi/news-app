c598539b2c0422fcb447819921208e74b39f4bd0
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-03-31 18:32:10 -0600

    Add app logo to Navbar and update app name for branding consistency

--- client/components/Header/Navbar.tsx
+++ client/components/Header/Navbar.tsx
@@ -10,6 +10,7 @@ import useUser from "@/hooks/useUser";
 import UserLoggedIn from "../UserLoggedIn";
 import MobileSheet from "../MobileSheet";
 import { SearchBar } from "../SearchBar";
+import Image from "next/image";
 
 const Navbar = () => {
   const { user } = useUser();
@@ -17,12 +18,23 @@ const Navbar = () => {
   return (
     <header className="bg-accent rounded-md  shadow-md sticky top-0 z-50">
       <nav className="flex justify-between items-center p-4 ">
-        <div className="flex items-center justify-center gap-2">
-          {/* todo : Add app logo here */}
+        <div className="flex items-center gap-1">
+          {/* App Logo */}
+          <Link href={"/"}>
+            <Image
+              src={"app-logo.png"}
+              width={50}
+              height={50}
+              alt="App Logo"
+              className="p-1 rounded-md shadow-md dark:bg-accent-foreground bg-gray-300"
+            />
+          </Link>
+
+          {/* App Name */}
           <Link
-            className="capitalize md:text-2xl text-lg font-semibold"
-            href={"/"}>
-            Canadian News
+            href={"/"}
+            className="capitalize md:text-2xl text-lg font-semibold text-primary hover:text-primary-dark transition-colors">
+            Network
           </Link>
         </div>
 
@@ -31,6 +43,9 @@ const Navbar = () => {
           {categories.map((c) => (
             <CatagoryTitle key={c.name} name={c.name} />
           ))}
+          {process.env.NODE_ENV === "development" && (
+            <Link href={"/testing"}>Test</Link>
+          )}
         </div>
 
         {/* Desktop */}
@@ -50,6 +65,7 @@ const Navbar = () => {
           <MobileSheet user={user} />
         </div>
       </nav>
+      {/* Re enable after it has been fixed */}
       <SearchBar />
     </header>
   );
