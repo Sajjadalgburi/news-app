18abc2bdc35e8af0250d5fd34e8e45b7882835a5
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-02 10:10:26 -0600

    Update heading text in LandingPage component to reflect global news

--- client/components/homepage-stuff/LandingPage.tsx
+++ client/components/homepage-stuff/LandingPage.tsx
@@ -3,9 +3,9 @@ import HomePageNews from "./HomePageNews";
 
 const LandingPage = () => {
   return (
-    <section className=" py-8 px-4 max-w-7xl mx-auto">
-      <h2 className="md:text-4xl text-2xl font-bold text-left text-gray-800 mb-6">
-        Currently Trending News In Canada
+    <section className="py-8 px-4 max-w-7xl mx-auto">
+      <h2 className="md:text-4xl text-2xl font-bold text-left mb-6">
+        Currently Trending News Around The Globe
       </h2>
 
       <HomePageNews />
